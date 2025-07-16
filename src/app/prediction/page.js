e77c9b814658c0a2535d0237a7a9bb7923135350
Tanpreet <tanpreetjhally300@gmail.com>
2025-07-16 15:45:47 +0530

    working model

--- src/app/prediction/page.js
+++ src/app/prediction/page.js
@@ -1,22 +1,51 @@
-"use client";
+// "use client";
+// import { Suspense } from 'react';
+// import { useSearchParams } from 'next/navigation';
+
+// export default function CropPredictor() {
+
+//   const searchParams = useSearchParams();
+//   const data = searchParams.get('data');
+
+
+//   return (
+//     <div className="min-h-screen flex flex-col items-center justify-center bg-green-50">
+//       <h1 className="text-3xl font-bold mb-6">🌾 KisaanMitra: Crop Recommendation</h1>
+
+//       {data && (
+//         <div className="mt-6 text-xl text-green-800 font-semibold">
+//           ✅ Best Crop for You: <span className="underline">{data}</span>
+//         </div>
+//       )}
+
+//     </div>
+//   );
+// }
+"use client"
+import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 export default function CropPredictor() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Content />
+    </Suspense>
+  );
+}
 
+function Content() {
   const searchParams = useSearchParams();
   const data = searchParams.get('data');
 
-
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-green-50">
-      <h1 className="text-3xl font-bold mb-6">🌾 KisaanMitra: Crop Recommendation</h1>
+      <h1 className="text-3xl font-bold mb-6">🌾 KisaanMitra: Crop Predictor</h1>
 
       {data && (
         <div className="mt-6 text-xl text-green-800 font-semibold">
           ✅ Best Crop for You: <span className="underline">{data}</span>
         </div>
       )}
-
     </div>
   );
 }
